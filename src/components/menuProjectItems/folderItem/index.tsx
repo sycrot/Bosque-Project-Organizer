b92e9118e4da0672b5270c474906f1092b470f05
Thiago Farias <thiagojosef99@gmail.com>
2024-11-12 09:07:37 -0300

    fix: error

--- src/components/menuProjectItems/folderItem/index.tsx
+++ src/components/menuProjectItems/folderItem/index.tsx
@@ -79,7 +79,8 @@ export default function FolderItem(props: IFolderItemProps) {
     idFolder: props.folder.id,
     title: '',
     stages: [],
-    color: ''
+    color: '',
+    workingTime: 0
   }
 
   return (
